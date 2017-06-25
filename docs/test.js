dfea74c25988d237212279ba388fe77bddaa974c
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-25 16:03:32 +0900

    commit

--- docs/test.js
+++ docs/test.js
@@ -1,13 +1,30 @@
-navigator.mediaDevices.enumerateDevices().then(devices => {
-    devices.filter(device => device.kind === 'videoinput').forEach(device => {
-        var opt = document.createElement('option');
-        opt.value = device.deviceId;
-        opt.textContent = `[WebCam] ${device.label}`;
-        captureSource.appendChild(opt);
-    })
-});
+
+var scenes = [];
+var currentSceneIdx = 0;
+
+btnAddSrc.onclick = evt => {
+    captureSource.style.top = '-10000px';
+    captureSource.innerHTML = '';
+    navigator.mediaDevices.enumerateDevices().then(devices => {
+        devices.filter(device => device.kind === 'videoinput').forEach(device => {
+            var opt = document.createElement('option');
+            opt.value = device.deviceId;
+            opt.textContent = `[WebCam] ${device.label}`;
+            captureSource.appendChild(opt);
+        });
+        var btnRect = btnAddSrc.getBoundingClientRect();
+        captureSource.style.display = '';
+        var listRect = captureSource.getBoundingClientRect();
+        if (btnRect.top - listRect.height - 2) {
+            captureSource.style.top = (btnRect.bottom + 2) + 'px';
+        } else {
+            captureSource.style.top = (btnRect.top - 2) + 'px';
+        }
+    });
+};
+
 btnStart.onclick = evt => {
-    if(vid.srcObject) {
+    if (vid.srcObject) {
         vid.srcObject.getTracks().forEach(track => track.stop());
     }
     if (['screen', 'window'].includes(captureSource.value)) {
